54af2215eab1133afed0ea286d4b197aed88ad40
Octavio Harris <harris.octavio@gmail.com>
2018-04-27 20:30:29 +0500

    Use Tkinter's after function to run email event loop alongside tkinter's
    Signed-off-by: Octavio Harris <harris.octavio@gmail.com>

--- app.py
+++ app.py
@@ -3,6 +3,10 @@
 import traceback
 import ConfigParser
 import Tkinter
+import subprocess
+#import threading
+
+root = None
 
 def exception_handler(text_segments, attachments):
 
@@ -13,24 +17,49 @@ def print_handler(text_segments, attachments):
     for text in text_segments:
         print(text)
 
-def win_open():
+def program_exit(text_segments, attachments):
 
-    root = Tkinter.Tk()
-    root.attributes('-fullscren', True)
-    root.mainloop()
-
-def win_close():
-
+    if root:
+        root.destroy()
+ 
     quit()
+
+def restart(text_segments, attachments):
+   
+    if root:
+        root.quit()
+
+    # Return false to stop the listener
+    return False
 
 def run(email_listener, settings):
 
-    email_listener.stop()
-    email_listener.register_handler('open', win_open)
-    email_listener.register_handler('close', win_close)
+    # Register the handlers for the types of actions
+    email_listener.register_handler('restart', restart)
+    email_listener.register_handler('exit', program_exit)
     email_listener.register_handler('print', print_handler)
     email_listener.register_handler('exception', exception_handler)
-    email_listener.start()
+   
+    # Initialize and open the Tkinter window
+    global root
+    root = Tkinter.Tk()
+    root.attributes('-fullscreen', True) 
+
+    # Function to poll email using Tkinter's after function
+    def poll_email():
+        
+        if not email_listener.running: return
+
+        root.after(int(email_listener.poll_interval * 1000), poll_email)
+        email_listener.process_latest_email()
+
+    # Set the email_listener to running
+    email_listener.running = True
+    
+    # Start polling email and enter main event loop
+    root.after(0, poll_email)
+    root.mainloop()
+    root.destroy()
 
 def main():
     
